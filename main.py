4629ef6add3d137a8c130252d937f54e0563a3f9
brandondjango <brandonlockridge@gmail.com>
2020-10-24 10:58:42 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+class CartesianSet:
+    def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+print "Enter number of points you want: \n"
+number_of_points = input()
+list = []
+for i in range(0, number_of_points):
+    print "\nEnter x coordinate: \n"
+    x = input()
+    print "\nEnter y coordinate: \n"
+    y = input()
+
+    cartesian_set = CartesianSet(x, y)
+    list.append(cartesian_set)
+
+
+
+for i in list:
+    print ("x: " + str(i.x) + " y: " + str(i.y) + "\n")
+
+count = 0
+
+for point in list:
+    for point2 in list:
+        if point.x == point2.x and point.y < point2.y:
+            count+=1
+print "\n" + str(count)
+
+
+
