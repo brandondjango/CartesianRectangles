a48ce3421eaf310bebc86f9502031c61a23e62e3
brandondjango <brandonlockridge@gmail.com>
2020-10-26 11:27:43 -0400

    fix

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ class CartesianSet:
             self.x = x
             self.y = y
 
-print "Enter number of points you want: \n"
-number_of_points = input()
+#print "Enter number of points you want: \n"
+#number_of_points = input()
 list = []
-for i in range(0, number_of_points):
-    print "\nEnter x coordinate: \n"
-    x = input()
-    print "\nEnter y coordinate: \n"
-    y = input()
+#for i in range(0, number_of_points):
+#    print "\nEnter x coordinate: \n"
+#    x = input()
+#    print "\nEnter y coordinate: \n"
+#    y = input()
+#
+#    cartesian_set = CartesianSet(x, y)
+#    list.append(cartesian_set)
 
-    cartesian_set = CartesianSet(x, y)
-    list.append(cartesian_set)
+for i in range(0, 4):
+    for j in range(0, 3):
+        cartesian_set = CartesianSet(i, j)
+        list.append(cartesian_set)
 
 
 
@@ -21,10 +26,14 @@ for i in list:
     print ("x: " + str(i.x) + " y: " + str(i.y) + "\n")
 
 count = 0
+answer = 0
 
 for point in list:
+
     for point2 in list:
+
         if point.x == point2.x and point.y < point2.y:
+            answer+=count
             count+=1
 print "\n" + str(count)
 
